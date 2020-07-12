2afb68bf54103e26a03f9171ed8e66108c8ad497
Dimitar Parapanov <d.parapanov@abv.bg>
2020-07-12 22:16:02 +0300

    Add custom weapons and armors

---
+++ Sources/SwiftFMIRPCore/CustomArmors.swift
@@ -0,0 +1,29 @@
+struct FireWings: Armor {
+    var attack: Int = 5
+    var defence: Int = 30
+}
+
+struct DragonArmor: Armor {
+    var attack: Int = 0
+    var defence: Int = 25
+}
+
+struct LargeShield: Armor {
+    var attack: Int = 1
+    var defence: Int = 10
+}
+
+struct SteelBoots: Armor {
+    var attack: Int = 0
+    var defence: Int = 5
+}
+
+struct DiamondPlatemail: Armor {
+    var attack: Int = 2
+    var defence: Int = 20
+}
+
+struct DragonPlatemail: Armor {
+    var attack: Int = 2
+    var defence: Int = 35
+}
